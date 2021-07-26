10a9c4ba5ab1befea24e8fdf5a838b48ebfb79b5
Dmitry <upssq@mail.ru>
2021-07-26 18:37:13 +0300

    Оптимизировал слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-shadow */
+/* eslint-disable id-length */
 const body = document.querySelector('body');
 const popapMenu = body.querySelector('.popap-menu');
 const headerMenu = body.querySelector('.header__wrapper-menu');
@@ -10,12 +13,10 @@ const popapForm = body.querySelector('.popap-form');
 const popapFormClose = popapForm.querySelector('.popap-form__close');
 const popapPhoneField = popapForm.querySelectorAll('.popap-form__form-input--phone');
 const popapMailField = popapForm.querySelectorAll('.popap-form__form-input--mail');
-const popapInputs = popapForm.querySelectorAll('.popap-form__form-input');
 const pricesCartCutton = body.querySelectorAll('.prices__cart-button');
 const questionsForm = body.querySelector('.questions__form');
 const questionsPhoneField = questionsForm.querySelector('.questions__form-input--phone');
 const questionsMailField = questionsForm.querySelector('.questions__form-input--mail');
-const questionsFields = questionsForm.querySelector('.questions__form-input');
 const avatarTravels = body.querySelectorAll('.travels__item');
 const travelLinks = body.querySelectorAll('.travel-places__link');
 const travelCarts = body.querySelectorAll('.travel-places__item-cart');
@@ -24,6 +25,8 @@ const success = body.querySelector('#success')
   .content
   .querySelector('.success');
 
+const cartActive = 'travel-places__item-cart--active';
+const buttonActive = 'travel-places__link--active';
 const successElement = success.cloneNode(true);
 
 headerMenu.classList.add('d-block');
@@ -46,27 +49,38 @@ const onBuyTour = (evt) => {
   evt.preventDefault();
   popapForm.classList.remove('d-none');
   overlay.classList.remove('d-none');
-}
+};
 
 const onCloseBuyTour = (evt) => {
   evt.preventDefault();
   popapForm.classList.add('d-none');
   overlay.classList.add('d-none');
-}
+};
 
-for (let popapLink of popapLinks) {
+for (const popapLink of popapLinks) {
   popapLink.addEventListener('click', onClickRemoveMenu);
 }
 
-for (let button of buyTourButtons) {
+for (const button of buyTourButtons) {
   button.addEventListener('click', onBuyTour);
 }
 
-for (let button of pricesCartCutton) {
+for (const button of pricesCartCutton) {
   button.addEventListener('click', onBuyTour);
 }
 
+// eslint-disable-next-line no-undef
 const isEscEvent = (evt) => evt.key === keys.escape || evt.key === keys.esc;
+
+const alertError = () => {
+  questionsPhoneField.classList.add('error');
+};
+
+
+const onSuccessRemove = () => {
+  successElement.remove();
+  document.removeEventListener('click', onSuccessRemove);
+};
 
 const onElementEscRemove = () => {
   if (isEscEvent) {
@@ -75,41 +89,32 @@ const onElementEscRemove = () => {
   }
 };
 
-const alertError = () => {
-  questionsFieldPhone.classList.add('error');
-}
-
-const onSuccessRemove = () => {
-  successElement.remove();
-  document.removeEventListener('click', onSuccessRemove);
-};
-
 const alertSuccess = () => {
   body.append(successElement);
   document.addEventListener('keydown', onElementEscRemove);
   document.addEventListener('click', onSuccessRemove);
-}
+};
 
 const resetForm = () => {
   questionsPhoneField.value = '';
   questionsMailField.value = '';
   popapPhoneField.value = '';
   popapMailField.value = '';
-}
+};
 
 const alertForm = () => {
   alertSuccess();
   resetForm();
-}
+};
 
-const sendData = (url, bodyForm, success, error) => {
+const sendData = (url, bodyForm, alertSucces, error) => {
   fetch(url, {
     method: 'POST',
     body: bodyForm,
   })
     .then((response) => {
       if (response.ok) {
-        success();
+        alertSucces();
       } else {
         error();
       }
@@ -127,31 +132,23 @@ const onFormSend = (evt) => {
   sendData(dataSabmitUrl, formData, alertForm, alertError);
 };
 
-for (let i = 0; i < avatarTravels.length; i++) {
-  avatarTravels[i].addEventListener('click', () => {
+const switchSlides = (avatarSwitchers, switchers, slides) => {
+  for (let i = 0; i < avatarSwitchers.length; i++) {
+    avatarSwitchers[i].addEventListener('click', () => {
 
-    for (let i = 0; i < avatarTravels.length; i++) {
-      travelCarts[i].classList.remove('travel-places__item-cart--active');
-      travelLinks[i].classList.remove('travel-places__link--active');
-    }
+      for (let i = 0; i < avatarSwitchers.length; i++) {
+        slides[i].classList.remove(cartActive);
+        switchers[i].classList.remove(buttonActive);
+      }
 
-    travelCarts[i].classList.add('travel-places__item-cart--active');
-    travelLinks[i].classList.add('travel-places__link--active');
-  });
-}
+      slides[i].classList.add(cartActive);
+      switchers[i].classList.add(buttonActive);
+    });
+  }
+};
 
-for (let i = 0; i < travelLinks.length; i++) {
-  travelLinks[i].addEventListener('click', () => {
-
-    for (let i = 0; i < avatarTravels.length; i++) {
-      travelCarts[i].classList.remove('travel-places__item-cart--active');
-      travelLinks[i].classList.remove('travel-places__link--active');
-    }
-
-    travelCarts[i].classList.add('travel-places__item-cart--active');
-    travelLinks[i].classList.add('travel-places__link--active');
-  });
-}
+switchSlides(avatarTravels, travelLinks, travelCarts);
+switchSlides(travelLinks, travelLinks, travelCarts);
 
 questionsForm.addEventListener('submit', onFormSend);
 popapForm.addEventListener('submit', onFormSend);
